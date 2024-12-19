c7fb47a094527002f172e8f35d9617c4637f5fa9
DmitryMorozov <d_s.morozov@yahoo.com>
2024-12-19 23:39:46 +0200

    Added users data

--- src/data/users.js
+++ src/data/users.js
@@ -1,7 +1,9 @@
 const users = [
     { id: 1, name: 'Alice', email: 'alice@example.com' },
     { id: 2, name: 'Bob', email: 'bob@example.com' },
-    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' },
+    { id: 4, name: 'Charlie', email: 'charlie@example.com' },
+    { id: 5, name: 'Charlie', email: 'charlie@example.com' }
 ];
 
 module.exports = users;
